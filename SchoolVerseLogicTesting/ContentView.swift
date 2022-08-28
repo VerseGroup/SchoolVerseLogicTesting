6c624fbce5ec5134e77e34f42093e0d92eaf0461
Steven Yu <stevenyuser@gmail.com>
2022-08-28 11:40:06 -0400

    Initial Commit

---
+++ SchoolVerseLogicTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchoolVerseLogicTesting
+//
+//  Created by Steven Yu on 8/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SchoolVerseLogicTesting/SchoolVerseLogicTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolVerseLogicTestingApp.swift
+//  SchoolVerseLogicTesting
+//
+//  Created by Steven Yu on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolVerseLogicTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
